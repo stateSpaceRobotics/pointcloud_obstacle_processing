4116c7d82c916c82bf11093424bf69c848dffe5c
Unknown <gmf78@msstate.edu>
2019-01-26 17:46:55 -0600

    Working on Refactor

--- point_cloud_processing/include/ProcessingFunctions.h
+++ point_cloud_processing/include/ProcessingFunctions.h
@@ -30,30 +30,77 @@ namespace CloudProcessing
     //Transform the given point cloud using the given transform
     void Transform()
     {
-
+        
     }
 
     //Downsample the point cloud
-    void Downsample(const pcl::PointCloud<pcl::PointXYZ>::Ptr& inCloud, pcl::<pcl::PointXYZ>& outCloud, float leafSize, ros::Publisher &publisher, bool publish)
+    void Downsample(const pcl::PointCloud<pcl::PointXYZ>::Ptr& inCloud, pcl::<pcl::PointXYZ>& outCloud, float leafSize, ros::Publisher &publisher, bool publish = false)
     {
+        pcl::VoxelGrid<pcl::PointXYZ> sor;
+        sor.setInputCloud (input_cloud);
+        sor.setLeafSize(leaf_size, leaf_size, leaf_size);
+        sor.filter (output_cloud);
 
+        if (!publish) return;
     }
 
     //Remove any statistical outliers
-    void RemoveStatisticalOutliers(pcl::PointCloud<pcl::PointXYZ>::Ptr& inCloud, pcl::PointCloud<pcl::PointXYZ>& outCloud, float meanK, float stdDeviationThreshold, ros::Publisher &publisher, bool publish)
+    void RemoveStatisticalOutliers(pcl::PointCloud<pcl::PointXYZ>::Ptr& inCloud, pcl::PointCloud<pcl::PointXYZ>& outCloud, float meanK, float stdDeviationThreshold, ros::Publisher &publisher, bool publish = false)
     {
+        pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+        sor.setInputCloud(input_cloud);
+        sor.setMeanK(meanK);
+        sor.setStddevMulThresh(stdDevThres);
+        sor.filter(output_cloud);
 
+        if (!publish) return;
     }
 
     //Segment away the specified plane
-    void SegmentPlane()
+    void SegmentPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr& inCloud, pcl::PointCloud<pcl::PointXYZ>& outCloud, float distanceThreshold, ros::Publisher &publisher)
     {
+        pcl::SACSegmentation<pcl::PointXYZ> seg;
+        pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+        pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>());
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PCDWriter writer;
+        seg.setOptimizeCoefficients (true);
+        seg.setModelType (pcl::SACMODEL_PLANE);
+        seg.setMethodType (pcl::SAC_RANSAC);
+        seg.setMaxIterations (100);
+        seg.setDistanceThreshold (0.02);
 
-    }
+        int nr_points = (int)inCloud->points.size();
 
-    //Extract clusters of points
-    void ExtractEuclideanClusters()
-    {
+        *outCloud = *inCloud;
 
+        while (outCloud->points.size() > 0.3 * nr_points)
+        {
+            // Segment the largest planar component from the remaining cloud
+            seg.setInputCloud(outCloud);
+            seg.segment(*inliers, *coefficients);
+
+            if (inliers->indices.size () == 0)
+            {
+                std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+                break;
+            }
+
+            // Extract the planar inliers from the input cloud
+            pcl::ExtractIndices<pcl::PointXYZ> extract;
+            extract.setInputCloud(outCloud);
+            extract.setIndices(inliers);
+            extract.setNegative(false);
+
+            // Get the points associated with the planar surface
+            extract.filter(*cloud_plane);
+            //std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size() << " data points." << std::endl;
+
+            // Remove the planar inliers, extract the rest
+            extract.setNegative(true);
+            extract.filter(*cloud_f);
+            *outCloud = *cloud_f;
+        }
     }
 }
